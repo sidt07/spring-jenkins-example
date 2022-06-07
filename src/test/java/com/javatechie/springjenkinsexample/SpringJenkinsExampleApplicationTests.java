9b71cfcfe52da116c1209f33d1e5446cf41329ba
sidt07 <sidt07@gmail.com>
2022-06-07 15:46:01 +0530

    Failed version

--- src/test/java/com/javatechie/springjenkinsexample/SpringJenkinsExampleApplicationTests.java
+++ src/test/java/com/javatechie/springjenkinsexample/SpringJenkinsExampleApplicationTests.java
@@ -3,13 +3,16 @@ package com.javatechie.springjenkinsexample;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class SpringJenkinsExampleApplicationTests {
 	
-	public static org.slf4j.Logger logger=LoggerFactory.getLogger(SpringJenkinsExampleApplication.class);
+	//public static org.slf4j.Logger logger=LoggerFactory.getLogger(SpringJenkinsExampleApplication.class);
 	
 
 
